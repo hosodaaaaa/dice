278de041105a8847f3f1028b380f9d9893adfa31
細田敦史 <hosodaaaaa>
2020-12-28 20:09:40 +0900

    🔧  機能追加

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
-const printInNum = () => {
-  document.querySelector('button').addEventListener('click', () => {
-    const numbers = Math.floor(Math.random() * 6 + 1)
-    alert(numbers)
-  })
+/**
+ * この関数は入力された数字の中からランダムで数字を出力します
+ */
+
+class Dice {
+  /**
+    * @param {string} trigger 値を出力するイベントの発火要素
+    * @param {number} number 入力された数字が格納される
+   */
+  constructor(number) {
+    this.trigger = document.querySelector('button')
+    this.number = number
+  }
+
+  init() {
+    this.printNumber = this.printNumber.bind(this)
+    this.trigger.addEventListener('click', this.printNumber)
+  }
+
+  printNumber() {
+    if (isNaN(this.number)) {
+      alert('半角数字が入力されませんでした。ブラウザリロードしてください。')
+    } else {
+      const numbers = Math.floor(Math.random() * this.number + 1)
+      alert(`${numbers}番です!`)
+    }
+  }
 }
 
-window.addEventListener('DOMContentLoaded', printInNum)
+window.addEventListener('DOMContentLoaded', () => {
+  const typeValue = new Dice(parseInt(prompt('半角英数字を入力してください')))
+  typeValue.init()
+})
